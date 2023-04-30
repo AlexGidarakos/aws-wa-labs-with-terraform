a9d1688154ff5ca5a77ecf198588b72d6ab960c3
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 09:56:33 +0100

    Remove old paths

--- helpers/init.inc.sh
+++
@@ -1,6 +0,0 @@
-# Section start: base variables
-# These values affect resource names, tags, etc. generated from the template files
-PROJECT_OWNER="alexg"  # E.g. a person, team etc.
-PROJECT_NAME="aws-wa-labs-tf"
-PROJECT_REGION="us-east-1"
-# Section end: base variables
