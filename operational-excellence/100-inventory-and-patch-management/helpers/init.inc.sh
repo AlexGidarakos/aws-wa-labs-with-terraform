b6360c036885b0511fd9af900a9d87065f49634d
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 10:14:11 +0100

    Add LAB_NAME variable

--- operational-excellence/100-inventory-and-patch-management/helpers/init.inc.sh
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.inc.sh
@@ -2,5 +2,6 @@
 # These values affect resource names, tags, etc. generated from the template files
 PROJECT_OWNER="alexg"  # E.g. a person, team etc.
 PROJECT_NAME="aws-wa-labs-tf"
+LAB_NAME="operational-excellence/100-inventory-and-patch-management"
 PROJECT_REGION="us-east-1"
 # Section end: base variables
