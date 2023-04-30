b68031211e9d7c23b9edd7266e02d21d6386322f
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 11:27:57 +0100

    Create function to generate aws.config from template

--- operational-excellence/100-inventory-and-patch-management/helpers/init.inc.sh
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.inc.sh
@@ -5,3 +5,8 @@ PROJECT_NAME="aws-wa-labs-tf"
 LAB_NAME="operational-excellence/100-inventory-and-patch-management"
 PROJECT_REGION="us-east-1"
 # Section end: base variables
+
+# Section start: exit codes
+EXIT_OK=0
+ERROR_AWS_CONFIG_GENERATION=1
+# Section end: exit codes
--- operational-excellence/100-inventory-and-patch-management/helpers/init.sh
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 cd $(dirname "$0")
 source init.inc.sh
+
+# Section start: function definitions
+function generate_aws_config_from_template {
+  SED_FAILED="false"
+  echo "Processing \"aws.config.template\""
+  sed \
+    -e "s/PROJECT_REGION/$PROJECT_REGION/g" \
+    ../templates/aws.config.template > ../aws.config || SED_FAILED="true"
+
+  if [[ "$SED_FAILED" == "true" ]]; then
+    echo "Error generating \"aws.config\" from template, aborting"
+    exit $ERROR_AWS_CONFIG_GENERATION
+  else
+    echo "\"aws.config\" created successfully from template"
+  fi
+}
+# Section end: function definitions
+
+# Section start: script main block
+# Generate AWS configuration file from template
+generate_aws_config_from_template
+# Section end: script main block
