f764e2a518d7e3eea2489a8c78cbe6a8d6de87d0
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 11:40:03 +0100

    Create logging function

--- operational-excellence/100-inventory-and-patch-management/helpers/init.sh
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.sh
@@ -3,18 +3,30 @@ cd $(dirname "$0")
 source init.inc.sh
 
 # Section start: function definitions
+# Logging function with ISO8601 timestamps and severity level support
+function Log () {
+  if [[ $# -eq 1 ]]; then
+    EVENT="[INFO] $1"
+  elif [[ $# -eq 2 ]]; then
+    EVENT="[$1] $2"
+  fi
+
+  echo $(date +%Y-%m-%dT%H:%M:%S.%3N%:z) $EVENT
+}
+
+# Generate AWS configuration file from template
 function generate_aws_config_from_template {
   SED_FAILED="false"
-  echo "Processing \"aws.config.template\""
+  Log "Processing \"aws.config.template\""
   sed \
     -e "s/PROJECT_REGION/$PROJECT_REGION/g" \
     ../templates/aws.config.template > ../aws.config || SED_FAILED="true"
 
   if [[ "$SED_FAILED" == "true" ]]; then
-    echo "Error generating \"aws.config\" from template, aborting"
+    Log FATAL "Error generating \"aws.config\" from template, aborting"
     exit $ERROR_AWS_CONFIG_GENERATION
   else
-    echo "\"aws.config\" created successfully from template"
+    Log "\"aws.config\" created successfully from template"
   fi
 }
 # Section end: function definitions
