685b44c994eba5f705b8eea8eff630c85045999b
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 12:26:45 +0100

    Create function to generate aws.credentials from template

--- operational-excellence/100-inventory-and-patch-management/helpers/init.sh
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.sh
@@ -23,10 +23,37 @@ function generate_aws_config_from_template {
     ../templates/aws.config.template > ../aws.config || SED_FAILED="true"
 
   if [[ "$SED_FAILED" == "true" ]]; then
-    Log FATAL "Error generating \"aws.config\" from template, aborting"
+    Log FATAL "Could not generate \"aws.config\" from template, aborting"
     exit $ERROR_AWS_CONFIG_GENERATION
   else
     Log "\"aws.config\" created successfully from template"
+  fi
+}
+
+# Generate AWS credentials file from template
+function generate_aws_credentials_from_template {
+  if [[ -z ${AWS_ACCESS_KEY_ID+x} ]]; then
+    Log FATAL "Environment variable AWS_ACCESS_KEY_ID is not set, aborting"
+    exit $ERROR_AWS_ACCESS_KEY_ID_NOT_SET
+  fi
+
+  if [[ -z ${AWS_SECRET_ACCESS_KEY+x} ]]; then
+    Log FATAL "Environment variable AWS_SECRET_ACCESS_KEY is not set, aborting"
+    exit $ERROR_AWS_SECRET_ACCESS_KEY_NOT_SET
+  fi
+
+  SED_FAILED="false"
+  Log "Processing \"aws.credentials.template\""
+  sed \
+    -e "s/AWS_ACCESS_KEY_ID/$AWS_ACCESS_KEY_ID/g" \
+    -e "s/AWS_SECRET_ACCESS_KEY/$AWS_SECRET_ACCESS_KEY/g" \
+    ../templates/aws.credentials.template > ../aws.credentials || SED_FAILED="true"
+
+  if [[ "$SED_FAILED" == "true" ]]; then
+    Log FATAL "Could not generate \"aws.credentials\" from template, aborting"
+    exit $ERROR_AWS_CREDENTIALS_GENERATION
+  else
+    Log "\"aws.credentials\" created successfully from template"
   fi
 }
 # Section end: function definitions
@@ -34,4 +61,6 @@ function generate_aws_config_from_template {
 # Section start: script main block
 # Generate AWS configuration file from template
 generate_aws_config_from_template
+# Generate AWS credentials file from template
+generate_aws_credentials_from_template
 # Section end: script main block
