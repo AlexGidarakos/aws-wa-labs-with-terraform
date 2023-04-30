71aad8f31446fd8261d146b0facaa476269881fb
Alexandros Gidarakos <algida79@gmail.com>
2023-04-30 09:51:09 +0100

    Create subdirectory structure for multiple labs

---
+++ operational-excellence/100-inventory-and-patch-management/helpers/init.inc.sh
@@ -0,0 +1,6 @@
+# Section start: base variables
+# These values affect resource names, tags, etc. generated from the template files
+PROJECT_OWNER="alexg"  # E.g. a person, team etc.
+PROJECT_NAME="aws-wa-labs-tf"
+PROJECT_REGION="us-east-1"
+# Section end: base variables
